3c11d9cd9936c2d2b18075281a284c07e82c4d8c
GlenTrex <henrysvp@gmail.com>
2020-03-09 18:19:14 -0700

    Add files via upload

--- designs.js
+++ designs.js
@@ -1,40 +1,38 @@
-    //Variables
-    // Select color input
-    const colorInput = $('#colorPicker');
-    // Select size input
-    const inputHeight = $('#inputHeight');
-    const inputWidth = $('#inputWidth');
-    const canvas = $('#pixelCanvas');
-    // Select Canvas
-    //Form
-    const sizePicker = $('.sizePicker');
-    //Functions 
-    function makeGrid() {
-        let height = inputHeight.val();
-        let width = inputWidth.val();
-        canvas.innerHTML = '';
-        let tbody = document.createElement('tbody');
-        for (let x = 0; x < height; x++) {
-            let tr = document.createElement('tr');
-            for (let z = 0; z < width; z++) {
-                let td = document.createElement('td');
-                td.append(document.createTextNode(''));
-                tr.append(td);
-            }
-            tbody.append(tr);
+//Variable declaration 
+var canvas = document.getElementById("pixelCanvas");
+var height = document.getElementById("inputHeight");
+var width = document.getElementById("inputWidth");
+var sizePicker = document.getElementById("sizePicker");
+var color = document.getElementById("colorPicker");
+
+color.addEventListener("click", function(){});
+
+sizePicker.onsubmit = function(event){
+    event.preventDefault();
+    clearGrid();
+    makeGrid();
+};
+// makeGrid function. draw grid base on row and column size
+function makeGrid() {
+    for (let r=0; r<height.value; r++){
+        const row = canvas.insertRow(r);
+        for (let c=0; c<width.value; c++){
+            const cell = row.insertCell(c);
+            cell.addEventListener("click", fillSquare);
         }
-        canvas.empty().append(tbody);
-    };
-    //Change pixel color
-    function pixelClick(event) {
-        let color = colorInput;
-        $('body').on('click', 'td', function () {
-            $(this).css('background-color', color.val());
-        });
     }
-    // When size is submitted by the user, call makeGrid()
-    document.querySelector('form').addEventListener('submit', function (event) {
-        makeGrid();
-        pixelClick();
-        event.preventDefault();
-    });
+}
+
+//fill grid color functions 
+function fillSquare () {
+    this.setAttribute("style", `background-color: ${color.value}`);
+}
+
+// clear grid after re-clicking resubmit to start a new grid
+function clearGrid(){
+    while (canvas.firstChild){
+         canvas.removeChild(canvas.firstChild);
+    }
+}
+
+
